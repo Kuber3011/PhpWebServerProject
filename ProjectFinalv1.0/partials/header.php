faaf6b95db9daaf0c2f71a80bc7c2e2a428ebb9f
Kuber Singh <75132417+Kuber3011@users.noreply.github.com>
2023-04-10 18:18:40 -0400

    Delete ProjectFinalv1.0 directory

--- ProjectFinalv1.0/partials/header.php
+++
@@ -1,6 +0,0 @@
-<header>
-  <nav>
-    <a href="../pages/history.php">History</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="../pages/logout.php">Logout</a>        
-  </nav>
-</header>
